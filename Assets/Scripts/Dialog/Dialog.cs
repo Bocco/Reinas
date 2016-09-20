ae0e0bdfacd2264e05efac7a5801a8afa8889bbf
Andrés <andrescevallosl@gmail.com>
2016-09-20 16:03:13 -0500

    Inicio Funcionalidad Dialogo
    Escena de prueba de dialogos.
    Creación de Dialogo Inicio

---
+++ Assets/Scripts/Dialog/Dialog.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class Dialog : MonoBehaviour {
+
+
+	private Text _textComponent;
+	public string[] stringarray;
+	public float secondsbetween = 0.5f;
+	// Use this for initialization
+	void Start () {
+		_textComponent = GetComponent<Text>();
+		_textComponent.text = "";
+	}
+	// Update is called once per frame
+	void Update () {
+			if(Input.GetMouseButtonDown(0)){
+				StartCoroutine(displayString("Hola esto es una pequeña prueba"));
+			}
+	
+	}
+
+		private IEnumerator displayString(string todisplay){
+			int stringlenght = todisplay.Length;
+			int index = 0;
+			_textComponent.text = "0";
+
+
+			while(index < stringlenght){
+				_textComponent.text += todisplay[index];
+				index++;
+				if(index < stringlenght){
+					yield return new WaitForSeconds(secondsbetween);
+
+				}
+			}
+
+		}
+}
